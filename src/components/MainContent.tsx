bcf05c42541427c2ca1147cc4e952525495d1f8a
Jo <joely.rooke@gmail.com>
2021-11-09 17:25:45 +0000

    level 1 completed, conditional rendering of gendered names

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,5 @@
 import Name from "./Name";
 import names from "../names.json";
-import { NameProps } from "../NameProps";
 
 export default function MainContent(): JSX.Element {
   const sortedNames = names.sort((a, b) => (a.name < b.name ? -1 : 1));
