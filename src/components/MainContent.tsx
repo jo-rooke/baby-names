15481dcc69e8cfbc7722c1c2a9e9f8d7d38e8f3e
Jo <joely.rooke@gmail.com>
2021-11-10 12:15:19 +0000

    level 2, search bar functional

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,19 +1,32 @@
 import Name from "./Name";
 import names from "../names.json";
+import { useState } from "react";
+import { NameProps } from "../NameProps";
 
 export default function MainContent(): JSX.Element {
+  const [searchTerm, setSearchTerm] = useState("");
   const sortedNames = names.sort((a, b) => (a.name < b.name ? -1 : 1));
 
+  const filteredNames: NameProps[] = sortedNames.filter((name) => {
+    return name.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
-    <div className="name">
-      {sortedNames.map((eachName) => (
-        <Name
-          key={eachName.id}
-          id={eachName.id}
-          name={eachName.name}
-          sex={eachName.sex}
-        />
-      ))}
-    </div>
+    <>
+      <input
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search for a name..."
+      />
+      <div className="name">
+        {filteredNames.map((eachName) => (
+          <Name
+            key={eachName.id}
+            id={eachName.id}
+            name={eachName.name}
+            sex={eachName.sex}
+          />
+        ))}
+      </div>
+    </>
   );
 }
