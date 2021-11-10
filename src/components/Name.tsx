59476d8a97e8f1971b37ab29f4c0823cab2bdd62
Jo <joely.rooke@gmail.com>
2021-11-10 12:25:45 +0000

    level 2, minor style changes

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -2,19 +2,6 @@ import { NameProps } from "../NameProps";
 import "./stylesheet.css";
 
 export default function Name(props: NameProps): JSX.Element {
-  const mStyle = {
-    backgroundColor: " #e0ccff",
-    color: "#5c00e6",
-    border: "2px",
-    margin: "3px",
-  };
-  const fStyle = {
-    backgroundColor: " #ffdb99",
-    color: "#b35900",
-    border: "2px",
-    margin: "3px",
-  };
-
   return (
     <>
       {props.sex === "m" ? (
